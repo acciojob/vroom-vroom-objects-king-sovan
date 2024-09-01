a7b5d05481815503feac39360604217ef9a0c994
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-09-01 19:35:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function SportsCar(make, model, topSpeed) {
 }
 
 SportsCar.prototype = Object.create(Car.prototype);
-SportsCar.protptype.constructor = SportsCar;
+SportsCar.prototype.constructor = SportsCar;
 
 SportsCar.prototype.getTopSpeed = function () {
 	return this.topSpeed;
